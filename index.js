ef0f5d50e3696ac8a5792b2a08a4655895651a72
Dima <dmytrotus@gmail.com>
2020-12-03 11:52:08 +0100

    make without cache

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ const client = redis.createClient(REDIS_PORT);
 
 const app = express();
 
-///make initial page
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+///play with code page
+/*app.get('/:name', (req, res) => {
+  res.send(myHelloPage(req.params.name));
 })
+function myHelloPage(name){
+	return `Імя ${name} є найкращим`;
+}*/
+
+//initial page
+app.get('/', (req,res) => {
+	res.send('Hello world');
+});
 
 //make request to Github to data
 async function getRepos(req, res, next) {
@@ -21,11 +29,19 @@ async function getRepos(req, res, next) {
 		const { username } = req.params;
 		const response = await fetch(`https://api.github.com/users/${username}`);
 		const data = await response.json();
-		res.send(data);
+		const repos = data.public_repos;
+		//res.send(data);
+		//set data to redis
+		client.setex(username, 3600, repos);
+		res.send(setResponse(username, repos));
 	} catch(err) {
 		console.error(err);
 		res.status(500);
 	}
+}
+//set reponse
+function setResponse(username, repos){
+	return `<h3>Юзер ${username} має ${repos} репозиторіїв на Github</h3>`
 }
 
 app.get('/repos/:username', getRepos);
